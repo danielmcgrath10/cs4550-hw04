76c14a096ba685876cad96a570aefa9ef64651b5
Daniel McGrath <danmcgrath1035@gmail.com>
2021-02-12 15:01:41 -0500

    big changes

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -4,26 +4,93 @@ defmodule Practice.Calc do
     num
   end
 
+  def getPrec(char) do 
+    cond do 
+      char == "+" || char == "-" -> 
+        1
+      char == "*" || char == "/" ->
+        2
+      char == "^" ->
+        3
+      true -> 
+        0
+    end
+  end
+
+  def checkOp(_prevOp, curOp, operators, postfix, check1, check2, ops) when check1 <= check2 do
+    inPos(ops -- [curOp], postfix, [curOp | operators])
+  end
+
+  def checkOp(prevOp, curOp, operators, postfix, _check1, _check2, ops) do
+    if Enum.empty?(operators) do
+      inPos(ops -- [curOp], postfix, [curOp | operators])
+    else
+      checkOp(prevOp, curOp, operators -- [prevOp], postfix ++ [prevOp], getPrec(prevOp), getPrec(curOp), ops)
+    end
+  end
+
+  def inPos(ops, postfix, operators) do
+    if Enum.empty?(ops) do
+      postfix ++ operators
+    else
+      curOp = Enum.at(ops, 0)
+      if String.match?(curOp, ~r/[-+\/*]/) do
+        prevOp = Enum.at(operators, 0)
+        checkOp(prevOp, curOp, operators, postfix, getPrec(prevOp), getPrec(curOp), ops)
+      else
+        inPos(ops -- [curOp], postfix ++ [curOp], operators)
+      end
+    end
+  end
+
+  def toPre(expr, prefix, nums) do
+    if length(expr) <= 1 do 
+      prefix
+    else
+      val = Enum.at(expr, 0)
+      if String.match?(val, ~r/[-+\/*]/) do
+        toPre(expr -- [val], prefix ++ [val, Enum.at(nums, 0), Enum.at(nums,1)], nums -- [Enum.at(nums, 0), Enum.at(nums,1)])
+      else
+        toPre(expr -- [val], prefix, [val | nums])
+      end
+    end
+  end
+
   def calc(expr) do
     # This should handle +,-,*,/ with order of operations,
     # but doesn't need to handle parens.
-    # expr
-    # |> String.split(~r/\s+/)
-    # |> Enum.map(fn(a, i) -> 
-    #   if String.match(a, ~r/-+\/*/) do
-    #     {:op, a}
-    #   else if String.match(a, ~r/[0-9]+/) do
-    #     {:num, a}
+    postfix = []
+    operators = []
+    ops = expr
+    |> String.split(~r/\s+/)
+    # |> Enum.map(fn(a) ->
+    #   IO.inspect a
+    #   cond do
+    #     String.match?(a, ~r/[-+\/*]/) ->
+    #       {:op, a}
+    #     String.match?(a, ~r/[0-9]+/) ->
+    #       {:num, a}
     #   end
     # end)
-    # |> hd
-    # |> parse_float
-    # |> :math.sqrt()
-    if !Regex.match?( ~r/[^+-\/*]||[a-zA-Z]/, expr) do
-      expr |> Code.eval_string |> elem(0)
-    else
-      false
-    end
+    |> IO.inspect
+
+    postFix = inPos(ops, postfix, operators)
+    |> IO.inspect
+
+    prefix = []
+    nums = []
+    preFix = toPre(postFix, prefix, nums)
+    |> IO.inspect
+    |> hd
+    |> parse_float
+    |> :math.sqrt()
+
+    # if !Regex.match?( ~r/[^+-\/*]||[a-zA-Z]/, expr) do
+    #   expr |> Code.eval_string |> elem(0)
+    # else
+    #   false
+    # end
+
     # Hint:
     # expr
     # |> split
